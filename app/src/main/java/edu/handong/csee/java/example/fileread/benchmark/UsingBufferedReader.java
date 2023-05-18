d855dba7e4ae1b9471f22ebb629c3bd70232d95b
seonmyeongjin <smj22200376@handong.ac.kr>
2023-05-18 10:58:17 +0900

    Initiate Commit

--- app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ app/src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -13,7 +13,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input100MB));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -26,7 +26,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input10MB));
+        BufferedReader inputStream2= new BufferedReader(new FileReader(DumpDataWriter.input1000MB));
         while (inputStream2.read()!=-1){}
 
         long duration2 = StopWatch.stop();
